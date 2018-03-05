3757e7e9d64cf049ff2b7715842f96a209dfd912
lmkirvan <kirvan.lewis@gmail.com>
2018-03-04 22:27:59 -0500

    nearly working find function

--- R/find.R
+++ R/find.R
@@ -1,5 +1,32 @@
+#rie_find <- function(document, trie){
+# characters <- purrr::flatten_chr(str_split(document, ""))
+# index <- seq_along(characters)
+# result <- list()
+# for(i in index){
+#   character <- characters[i]
+#   if(character %in% names(trie$children)){
+#     trie <- trie[[character]]
+#     result[[i]] <- list(
+#       value = trie$value,
+#       output = trie$output,
+#       index = i)
+#   } else {
+#     trie <- trie$fail
+#     if(character %in% names(trie$children)){
+#       trie <- trie[[character]]
+#       result[[i]] <- list(
+#         value = trie$value,
+#         output = trie$output,
+#         index = i)
+#     } else {
+#       NULL
+#     }
+#   }
+# }
+# result
+
 trie_find <- function(document, trie){
-  characters <- purrr::flatten_chr(str_split(document, ""))
+  characters <- purrr::flatten_chr(stringr::str_split(document, ""))
   index <- seq_along(characters)
   result <- list()
   for(i in index){
@@ -7,24 +34,44 @@ trie_find <- function(document, trie){
     if(character %in% names(trie$children)){
       trie <- trie[[character]]
       result[[i]] <- list(
-        value = trie$value,
-        output = trie$output,
+        outputs = get_attribute(trie, "output"),
+        value = get_attribute(trie, "value"),
         index = i)
-    } else {
-      trie <- trie$fail
-      if(character %in% names(trie$children)){
-        trie <- trie[[character]]
-        result[[i]] <- list(
-          value = trie$value,
-          output = trie$output,
-          index = i)
+      } else if(!is.null(trie$fail)){
+        trie <- trie$fail
+        if(character %in% names(trie$children)){
+          trie <- trie[[character]]
+          result[[i]] <- list(
+            outputs = get_attribute(trie, "output"),
+            value = get_attribute(trie, "value"),
+            index = i)
+          }
       } else {
-        NULL
+        if(is.null(trie$output)){
+          result[[i]] <- list(
+            outputs = get_attribute(trie, "output"),
+            value = get_attribute(trie, "value"),
+            index = i)
+        }
       }
     }
-  }
   result
-}
+  }
 
+#problem with getting stuck at rootnode and not progressing forward.
+get_attribute <- function(self, attribute) {
+  if(is.null(self[[attribute]])){
+    NULL
+  } else {
+    this <- self[[attribute]]
+    c(this, get_attribute(self$fail, attribute))
+    }
+  }
 
+keys <- c("a", "ab", "bab", "bc", "bca", "c", "caa")
+values <- c("foo", "bar", "foo", "bar", "foo", "bar", "foo")
+example_trie <- trie_create(keys = keys, value = values)
+find_string <- "abccab"
+temp <- trie_find(find_string, example_trie)
 
+example_trie$c$fail
--- R/utilities.R
+++ R/utilities.R
@@ -31,19 +31,3 @@ add_rootnodes <- function(trie){
 }
 
 
-.onLoad <- function(libname, pkgname) {
-  op <- options()
-  op.devtools <- list(
-    devtools.path = "~/R-dev",
-    devtools.install.args = "",
-    devtools.name = "Lewis Kirvan",
-    devtools.desc.author = "Lewis Kirvan lewis.kirvan@cfpb.gov [aut, cre]",
-    devtools.desc.license = "MIT",
-    devtools.desc.suggests = NULL,
-    devtools.desc = list()
-  )
-  toset <- !(names(op.devtools) %in% names(op))
-  if(any(toset)) options(op.devtools[toset])
-
-  invisible()
-}
